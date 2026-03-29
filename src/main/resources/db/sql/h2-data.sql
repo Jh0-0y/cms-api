5135955bc9012fc4a76d5add1e3282ea03f73293
jhy <idpc8090@gmail.com>
2026-03-29 15:34:40 +0900

    [fix] 초기 데이터 BCrypt 해시 수정

--- src/main/resources/db/sql/h2-data.sql
+++ src/main/resources/db/sql/h2-data.sql
@@ -4,8 +4,8 @@
 
 -- ADMIN 계정 (email: admin@malgn.com / password: admin123)
 insert into members (email, username, nickname, password, role)
-values ('admin@malgn.com', '관리자', 'admin', '$2a$10$3/ypADLMWAsBMCbbbYcYVO/lQJbF5x.AoZbvaomqPWwMjUdS2CJJG', 'ADMIN');
+values ('admin@malgn.com', '관리자', 'admin', '$2a$10$OpuG6DDrjsoO.F5cl9v7eO2eFbUe4AgE.dauLGfSZVOO9k0yLHTZy', 'ADMIN');
 
 -- USER 계정 (email: user@malgn.com / password: user123)
 insert into members (email, username, nickname, password, role)
-values ('user@malgn.com', '사용자', 'user', '$2a$10$LoaRyMZ2z4oGUWlE6/4Kp.6HovHeQ7ZiDUJHRSyBmpgMWbWjGwY7W', 'USER');
+values ('user@malgn.com', '사용자', 'user', '$2a$10$l6gIyllChJEHmnMJo/5DlOXn7hzDj7stuMf2P2hv0o8CiY26rFTpO', 'USER');
